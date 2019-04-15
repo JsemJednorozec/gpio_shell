d4cb1758e2b798743bbf22f99fc088462acec920
JsemJednorozec <jjezek@yandex.com>
2019-04-15 16:32:26 +0200

    code

---
+++ gpio.rb
@@ -0,0 +1,68 @@
+require 'io/console'
+require './RbWiringOP/wiring.rb'
+
+def print_help
+    puts <<EOF
+help        print this help
+out <pin>   set the pin as an output pin
+in <pin>    set the pin as an input pin
+hi <pin>    set the pin to high
+lo <pin>    set the pin to low
+reset       reset all the pins
+EOF
+end
+
+pins = []
+
+# command parser
+class Parser
+    def initialize
+    end
+
+    def parse string
+        #params = []
+        params = string.split
+        command = params.shift
+        exec command, params
+    end
+
+    def exec command, params
+        case command
+        when "help"
+            print_help
+        when "out"
+            pin_no = params[0].to_i
+            begin
+                pin[pin_no] = WiringOP::Pin.new :pin => pin_no, :direction => :out
+            rescue
+                puts "error"
+            end
+        when "in"
+            pin_no = params[0].to_i
+            begin
+                pin[pin_no] = WiringOP::Pin.new :pin => pin_no, :direction => :in
+            rescue
+                puts "error"
+            end
+        when "hi"
+            pin_no = params[0].to_i
+            pins[pin_no].set_hi
+        when "lo"
+            pin_no = params[0].to_i
+            pins[pin_no].set_lo
+        when "reset"
+            WiringOP.reset
+        else
+            puts "bad command"
+        end
+    end
+end
+
+p = Parser.new
+
+puts "enter 'help' for help"
+
+loop do 
+    print '>'
+    p.parse gets.chomp
+end
